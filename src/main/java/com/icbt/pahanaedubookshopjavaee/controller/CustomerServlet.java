14145e1a9de5839f3532ea053b32649a2631f135
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2025-07-04 11:41:38 +0530

    introduce customer servlet

---
+++ src/main/java/com/icbt/pahanaedubookshopjavaee/controller/CustomerServlet.java
@@ -0,0 +1,31 @@
+package com.icbt.pahanaedubookshopjavaee.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
