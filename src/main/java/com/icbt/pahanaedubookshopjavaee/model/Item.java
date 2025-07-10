f884ac937dcc1c5d85c6a5ba80bad72ca42dc5ec
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2025-07-10 20:15:23 +0530

    introduce item model class

---
+++ src/main/java/com/icbt/pahanaedubookshopjavaee/model/Item.java
@@ -0,0 +1,64 @@
+package com.icbt.pahanaedubookshopjavaee.model;
+
+import java.math.BigDecimal;
+
+public class Item {
+    private String itemCode;
+    private String itemName;
+    private BigDecimal unitPrice;
+    private Integer qtyOnHand;
+    private char status;
+
+    public Item() {}
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(Integer qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemCode='" + itemCode + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", qtyOnHand=" + qtyOnHand +
+                ", status=" + status +
+                '}';
+    }
+}
