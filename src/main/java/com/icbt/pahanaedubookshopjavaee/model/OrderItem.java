20868623aa42ae3201dc70661d803e3712190911
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2025-07-29 16:59:22 +0530

    implement place order operation

--- src/main/java/com/icbt/pahanaedubookshopjavaee/model/OrderItem.java
+++ src/main/java/com/icbt/pahanaedubookshopjavaee/model/OrderItem.java
@@ -1,4 +1,40 @@
 package com.icbt.pahanaedubookshopjavaee.model;
 
+import java.math.BigDecimal;
+
 public class OrderItem {
+    private String itemCode;
+    private int qty;
+    private BigDecimal unitPrice;
+    private BigDecimal discountApplied;
+    private BigDecimal lineTotal;
+
+    public OrderItem(String itemCode, int qty, BigDecimal unitPrice, BigDecimal discountApplied, BigDecimal lineTotal) {
+        this.itemCode = itemCode;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+        this.discountApplied = discountApplied;
+        this.lineTotal = lineTotal;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public BigDecimal getDiscountApplied() {
+        return discountApplied;
+    }
+
+    public BigDecimal getLineTotal() {
+        return lineTotal;
+    }
 }
+
--- src/main/java/com/icbt/pahanaedubookshopjavaee/service/impl/PlaceOrderServiceImpl.java
+++ src/main/java/com/icbt/pahanaedubookshopjavaee/service/impl/PlaceOrderServiceImpl.java
@@ -2,9 +2,12 @@ package com.icbt.pahanaedubookshopjavaee.service.impl;
 
 import com.icbt.pahanaedubookshopjavaee.dao.PlaceOrderDAO;
 import com.icbt.pahanaedubookshopjavaee.dao.impl.PlaceOrderDAOImpl;
+import com.icbt.pahanaedubookshopjavaee.model.OrderItem;
 import com.icbt.pahanaedubookshopjavaee.service.PlaceOrderService;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class PlaceOrderServiceImpl implements PlaceOrderService {
 
@@ -14,5 +17,9 @@ public class PlaceOrderServiceImpl implements PlaceOrderService {
         this.placeOrderDAO = new PlaceOrderDAOImpl(dataSource);
     }
 
+    @Override
+    public String placeOrder(String customerId, BigDecimal totalAmount, BigDecimal totalDiscount, List<OrderItem> orderItems) throws Exception {
+        return placeOrderDAO.createOrder(customerId, totalAmount, totalDiscount, orderItems);
+    }
 
 }
