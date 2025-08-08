24a82b30b6fc65fc1aef3179bb2816c3d2c89eb6
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2025-08-08 15:43:18 +0530

    rename username

--- src/main/java/com/icbt/pahanaedubookshopjavaee/util/constants/AuthConstants.java
+++ src/main/java/com/icbt/pahanaedubookshopjavaee/util/constants/AuthConstants.java
@@ -1,6 +1,6 @@
 package com.icbt.pahanaedubookshopjavaee.util.constants;
 
 public class AuthConstants {
-    public static final String ADMIN_USERNAME = "admin";
+    public static final String ADMIN_USERNAME = "Admin";
     public static final String ADMIN_PASSWORD = "1234";
 }
